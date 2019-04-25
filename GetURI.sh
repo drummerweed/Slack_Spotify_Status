e9bd58e60a54840969c0822fd73ff2364d0dbe6b
drummerweed <elisha.productions@gmail.com>
2019-04-25 10:56:55 -0700

    .

--- GetURI.sh
+++ GetURI.sh
@@ -5,5 +5,14 @@ TOKEN=$(<$TOKEN_LOC)
 
 
 uri=$(osascript -e 'tell application "Spotify" to spotify url of current track')
-URL="https://slack.com/api/chat.postMessage?token="${TOKEN}"&channel=%23spotify-playlist&text="${uri}"&as_user=Patrick&pretty=1"
+channel="music"
+URL="https://slack.com/api/chat.postMessage?token="${TOKEN}"&channel="${channel}"&text="${uri}"&as_user=patrick_weed&pretty=1"
 curl -s -d "payload=$json" $URL
+
+# webhook_url="Your Webhook URL Here"
+# channel="Slackbot"
+
+# escapedText=$(echo $text | sed 's/"/\"/g' | sed "s/'/\'/g" )
+# json="{\"channel\": \"$channel\", \"text\": \"$escapedText\"}"
+
+# curl -s -d "payload=$json" "$webhook_url"
